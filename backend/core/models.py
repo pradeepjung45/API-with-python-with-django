4395ef26390cdd07e1cd4fdefcf0d4a139014ab0
pradeep jung karki <90687887+pradeepjung45@users.noreply.github.com>
2023-05-29 15:13:21 +0545

    first commit

---
+++ backend/core/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from django.db import models
+from utils.model_abstracts import Model
+from django_extensions.db.models import (
+	TimeStampedModel, 
+	ActivatorModel,
+	TitleDescriptionModel
+)
+
+class Contact(
+	TimeStampedModel, 
+	ActivatorModel,
+	TitleDescriptionModel,
+	Model
+	):
+
+	class Meta:
+		verbose_name_plural = "Contacts"
+
+	email = models.EmailField(verbose_name="Email")
+
+	def __str__(self):
+		return f'{self.title}'
+# Create your models here.
